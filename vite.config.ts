d058ea68105d0c7749c5de1d04a46c020126d7fd
Batto199 <sibahle.boilerroom@gmail.com>
2025-06-04 06:04:33 +0200

    Add base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
+    base: '/cake-ordering-gateway/',
     define: {
       'process.env': {
         VITE_SUPABASE_URL: env.VITE_SUPABASE_URL,
